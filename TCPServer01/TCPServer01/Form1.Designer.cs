c44220dea1359f09b703d5b0158b3f0bb7b6473c
Nikola Milovanović <48221371+dzonnikola@users.noreply.github.com>
2020-08-06 00:16:44 +0200

    Add files via upload

---
+++ TCPServer01/TCPServer01/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TCPServer01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbConsoleOutput = new System.Windows.Forms.TextBox();
+            this.tbIPAdress = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbStartListening = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSendClient = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbConsoleOutput
+            // 
+            this.tbConsoleOutput.Location = new System.Drawing.Point(12, 12);
+            this.tbConsoleOutput.Multiline = true;
+            this.tbConsoleOutput.Name = "tbConsoleOutput";
+            this.tbConsoleOutput.ReadOnly = true;
+            this.tbConsoleOutput.Size = new System.Drawing.Size(669, 285);
+            this.tbConsoleOutput.TabIndex = 0;
+            // 
+            // tbIPAdress
+            // 
+            this.tbIPAdress.Location = new System.Drawing.Point(62, 312);
+            this.tbIPAdress.Name = "tbIPAdress";
+            this.tbIPAdress.Size = new System.Drawing.Size(172, 20);
+            this.tbIPAdress.TabIndex = 1;
+            this.tbIPAdress.Text = "192.168.1.104";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(240, 312);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPort.TabIndex = 2;
+            this.tbPort.Text = "23000";
+            // 
+            // tbStartListening
+            // 
+            this.tbStartListening.Location = new System.Drawing.Point(62, 338);
+            this.tbStartListening.Name = "tbStartListening";
+            this.tbStartListening.Size = new System.Drawing.Size(278, 23);
+            this.tbStartListening.TabIndex = 3;
+            this.tbStartListening.Text = "Start Listening";
+            this.tbStartListening.UseVisualStyleBackColor = true;
+            this.tbStartListening.Click += new System.EventHandler(this.tbStartListening_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP/Port:";
+            // 
+            // tbSendClient
+            // 
+            this.tbSendClient.Location = new System.Drawing.Point(535, 312);
+            this.tbSendClient.Name = "tbSendClient";
+            this.tbSendClient.Size = new System.Drawing.Size(146, 20);
+            this.tbSendClient.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(535, 338);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(146, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 389);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbSendClient);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbStartListening);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbIPAdress);
+            this.Controls.Add(this.tbConsoleOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbConsoleOutput;
+        private System.Windows.Forms.TextBox tbIPAdress;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Button tbStartListening;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbSendClient;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ TCPServer01/TCPServer01/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPServer01
+{
+    public partial class Form1 : Form
+    {
+        TcpListener mTCPListener;
+        TcpClient mTCPClient;
+        byte[] mRx;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbStartListening_Click(object sender, EventArgs e)
+        {
+            IPAddress ipaddr;
+            int nPort;
+
+            if(!int.TryParse(tbPort.Text, out nPort))
+            {
+                nPort = 23000;
+            }
+            if(!IPAddress.TryParse(tbIPAdress.Text, out ipaddr))
+            {
+                ipaddr = IPAddress.Any;
+            }
+
+            mTCPListener = new TcpListener(ipaddr, nPort);
+
+            mTCPListener.Start();
+
+            mTCPListener.BeginAcceptTcpClient(onCompleteAcceptTcpClient, mTCPListener);
+
+        }
+
+        void onCompleteAcceptTcpClient(IAsyncResult iar)
+        {
+            TcpListener tcpl = (TcpListener)iar.AsyncState;
+            try
+            {
+                mTCPClient = tcpl.EndAcceptTcpClient(iar);
+
+                printLine("Klijent konektovan.");
+
+                mRx = new byte[512];
+
+                mTCPClient.GetStream().BeginRead(mRx, 0, mRx.Length, onCompleteReadFromTCPClientStream, mTCPClient);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void onCompleteReadFromTCPClientStream(IAsyncResult iar)
+        {
+            TcpClient tcpc;
+            int nCountReadBytes = 0;
+            string strRecv;
+
+            try
+            {
+                tcpc = (TcpClient)iar.AsyncState;
+                nCountReadBytes = tcpc.GetStream().EndRead(iar);
+
+                if(nCountReadBytes == 0)
+                {
+                    MessageBox.Show("Klijent diskonektovan.");
+                    return;
+                }
+
+                strRecv = Encoding.ASCII.GetString(mRx, 0, nCountReadBytes);
+                printLine(strRecv);
+
+                mRx = new byte[512];
+
+                tcpc.GetStream().BeginRead(mRx, 0, mRx.Length, onCompleteReadFromTCPClientStream, tcpc);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void printLine(string _print)
+        {
+            tbConsoleOutput.Invoke(new Action<string>(doInvoke), _print);
+        }
+
+        public void doInvoke(string _print)
+        {
+            tbConsoleOutput.Text = _print + Environment.NewLine + tbConsoleOutput.Text;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            byte[] tx = new byte[512];
+
+            if (string.IsNullOrEmpty(tbSendClient.Text)) return;
+
+            try
+            {
+                if(mTCPClient != null)
+                {
+                    if (mTCPClient.Client.Connected)
+                    {
+                        tx = Encoding.ASCII.GetBytes(tbSendClient.Text);
+                        mTCPClient.GetStream().BeginWrite(tx, 0, tx.Length, onCompleteWriteToClientStream, mTCPClient);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                tbSendClient.Clear();
+            }
+        }
+
+        private void onCompleteWriteToClientStream(IAsyncResult iar)
+        {
+            try
+            {
+                TcpClient tcpc = (TcpClient)iar.AsyncState;
+                tcpc.GetStream().EndRead(iar);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
